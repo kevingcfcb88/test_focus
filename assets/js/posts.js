1aae1fa4e9bb1f53f04817130d2b6180293edfc7
Kevin <kevingcfcb88@gmail.com>
2019-09-02 22:47:35 -0600

    Adding semi-responsiveness using media queries

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,23 +1,24 @@
 $(document).ready(function () {
 
+    //Once the document has been loaded, the render will begin.
     renderUsers();
 
-    /** JQuery Calls */
-
-    $("#post").on("click", function () {
-        renderPosts();
-    });
-
-    /** Functions */
+    /**
+     * Function in charge of render the main view.
+     */
     function renderUsers() {
         const urlUsers= "https://jsonplaceholder.typicode.com/users";
+        //Calling the generic method to get the authors
         ajaxCall(urlUsers, function(json){
             $.each(json, function (id, value) {
                 const urlNameGender = "https://api.genderize.io/?name="+getSingleName(value.name);
+                //Calling the generic method to get the gender of the name
                 ajaxCall(urlNameGender, function(json){
+                    //Set the obtained picture for each name
                     var sDiv = "<img class='joe' src='//joeschmoe.io/api/v1/"+json.gender+"/random'><div id='"+id+"' class='user'>" + value.name + "</div>";
                     $("#content").append(sDiv);
                     const urlPostByUser = "https://jsonplaceholder.typicode.com/posts?userId="+id;
+                    //Calling the generic method to get the posts by userId
                     ajaxCall(urlPostByUser, function(jsonPost){
                         var sPosts = "<ul>";
                         for (i = 0; i < jsonPost.length; i++){
@@ -31,7 +32,13 @@ $(document).ready(function () {
         });
     }
 
+    /**
+     * 
+     * @param url The URL to request
+     * @param callback The function to be executed when the call has been successfull
+     */
     function ajaxCall(url, callback) {
+        $("#message").hide();
         $.ajax({
             // The URL for the request
             url: url,
@@ -52,14 +59,18 @@ $(document).ready(function () {
             })
             // Code to run regardless of success or failure;
             .always(function (xhr, status) {
-                //alert("The request is complete!");
+                //Show the success message and hide it after 5 seconds
+                $("#message").show();
+                setTimeout(function(){
+                    $("#message").hide();
+                }, 5000);
             });
     }
 
     /**
      * Function created to return the first names without titles
-     * sName Full Name
-     * return the First name without titles
+     * @param sName Full Name
+     * @return the First name without titles
      */
     function getSingleName(sName){
         var aName = sName.split(" ");
